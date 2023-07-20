fffd3826b1b5408736e2f72624250bdc62f75948
Andre Luiz Junqueira Poles <andre.poles@fatec.sp.gov.br>
2023-07-20 12:45:03 -0300

    Logica de xequemate

--- src/application/Main.java
+++ src/application/Main.java
@@ -18,38 +18,39 @@ public class Main {
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
 
+        while (!chessMatch.getCheckMate()) {
+            try {
+                UI.clearScreen();
+                UI.printMatch(chessMatch, captured);
+                System.out.println();
+                System.out.print("Origem: ");
+                ChessPosition source = UI.readChessPosition(sc);
 
-            while (true) {
-                try {
-                    UI.clearScreen();
-                    UI.printMatch(chessMatch, captured);
-                    System.out.println();
-                    System.out.print("Origem: ");
-                    ChessPosition source = UI.readChessPosition(sc);
+                boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces(), possibleMoves);
 
-                    boolean[][] possibleMoves = chessMatch.possibleMoves(source);
-                    UI.clearScreen();
-                    UI.printBoard(chessMatch.getPieces(), possibleMoves);
+                System.out.println();
+                System.out.print("Destino: ");
+                ChessPosition target = UI.readChessPosition(sc);
 
-                    System.out.println();
-                    System.out.print("Destino: ");
-                    ChessPosition target = UI.readChessPosition(sc);
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 
-                    ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-
-                    if(capturedPiece != null){
-                        captured.add(capturedPiece);
-                    }
-                }
-                catch(ChessException e){
-                    System.out.println(e.getMessage());
-                    sc.nextLine();
-                }
-                catch(InputMismatchException e){
-                    System.out.println(e.getMessage());
-                    sc.nextLine();
+                if(capturedPiece != null) {
+                    captured.add(capturedPiece);
                 }
             }
+            catch(ChessException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+            catch(InputMismatchException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+        }
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
 
     }
 }
--- src/application/UI.java
+++ src/application/UI.java
@@ -57,9 +57,15 @@ public class UI {
         printCapturedPieces(captured);
         System.out.println();
         System.out.println("Turno: " + chessMatch.getTurn());
-        System.out.println("Esperando jogador: " + chessMatch.getCurrentPlayer());
-        if(chessMatch.getCheck()){
-            System.out.println("XEQUE!");
+        if(!chessMatch.getCheckMate()) {
+            System.out.println("Esperando jogador: " + chessMatch.getCurrentPlayer());
+            if (chessMatch.getCheck()) {
+                System.out.println("XEQUE!");
+            }
+        }
+        else{
+            System.out.println("CHECKMATE!");
+            System.out.println("Vencedor: " + chessMatch.getCurrentPlayer());
         }
     }
 
